23669c25d8f3e3cd14514538c20cd3678c193b41
Dawid Marcinków <dawid.marcinkow@gmail.com>
2017-10-13 12:46:11 +0200

    first commit

---
+++ pl/coderslab/dawidm94/gameGuessNumber/Main1.java
@@ -0,0 +1,12 @@
+package pl.coderslab.dawidm94.gameGuessNumber;
+import java.util.Scanner;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Zgadnij liczbę: ");
+	}
+
+}
